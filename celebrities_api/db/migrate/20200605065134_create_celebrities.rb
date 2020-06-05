ef848ca3cc6263d48dc9089cb04cdc164b6bcd2a
Andrei <37360070+ahuiban@users.noreply.github.com>
2020-06-05 03:54:09 -0400

    updated

---
+++ celebrities_api/db/migrate/20200605065134_create_celebrities.rb
@@ -0,0 +1,11 @@
+class CreateCelebrities < ActiveRecord::Migration[6.0]
+  def change
+    create_table :celebrities do |t|
+      t.string :name
+      t.string :img
+      t.string :comments
+
+      t.timestamps
+    end
+  end
+end
